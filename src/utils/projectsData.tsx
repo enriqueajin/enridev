4f67cdd70c5c586af92234dffd694bcb79295424
Enrique Ajin <enriquesaon@gmail.com>
2024-11-11 14:56:31 +0100

    Create Projects component using projectsData.tsx. Add secondaryBlue, primaryGray and darkGray to tailwind.config.js. Add projects' png pictures.

---
+++ src/utils/projectsData.tsx
@@ -0,0 +1,33 @@
+type Project = {
+  title: string,
+  imageUrl: string,
+  siteUrl: string,
+  sourceCodeUrl: string
+}
+
+export const projectsData: Project[] = [
+  {
+    title: 'Fylo',
+    imageUrl: '../../fylo.png',
+    siteUrl: 'https://enriqueajin.github.io/FyloLayout/index.html',
+    sourceCodeUrl: 'https://github.com/enriqueajin/FyloLayout'
+  },
+  {
+    title: 'Platzi Video',
+    imageUrl: '../../platzi-video.png',
+    siteUrl: 'https://enriqueajin.github.io/PlatziVideoLayout/index.html',
+    sourceCodeUrl: 'https://github.com/enriqueajin/PlatziVideoLayout'
+  },
+  {
+    title: 'Batatabit',
+    imageUrl: '../../batatabit.png',
+    siteUrl: 'https://enriqueajin.github.io/MobileFirstProject/index.html',
+    sourceCodeUrl: 'https://github.com/enriqueajin/MobileFirstProject'
+  },
+  {
+    title: 'Movies',
+    imageUrl: '../../movies.png',
+    siteUrl: 'https://enriqueajin.github.io/MoviesLayout/index.html',
+    sourceCodeUrl: 'https://github.com/enriqueajin/MoviesLayout'
+  },
+]
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,12 @@ export default {
     extend: {
       colors: {
         primaryBlue: "#179AFC",
+        secondaryBlue: "#004AC0",
         darkBlue: "#061028",
         gradientPurple: "#260D52",
-        gradientDarkPurple: "#180F3A"
+        gradientDarkPurple: "#180F3A",
+        primaryGray: "#1E2021",
+        darkGray: "#18171D"
       }
     },
   },
