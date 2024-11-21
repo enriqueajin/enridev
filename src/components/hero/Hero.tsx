8cf766be5e2900fb9d016e188a620e1e428ea851
Enrique Ajin <enriquesaon@gmail.com>
2024-11-21 23:45:06 +0100

    Set Hero's contact button's href to #Contact.

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -10,7 +10,7 @@ export function Hero () {
             <span className='text-white'>I create innovative </span>
             <span className='text-primaryBlue'>software solutions</span>
           </h1>
-          <a href="#contact">
+          <a href="#Contact">
             <button className='bg-primaryBlue rounded-lg px-8 py-3 font-bold hover:-translate-y-1 duration-300 hover:scale-110'>
               Contact me
             </button>
