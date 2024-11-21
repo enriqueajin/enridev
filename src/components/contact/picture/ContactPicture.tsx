560618b8404aeda29060818c811e007b5df4ad96
Enrique Ajin <enriquesaon@gmail.com>
2024-11-21 21:46:09 +0100

    Create Contact.tsx component with its components. Handle form validation, submit and events with React Hook Form and zod. Set up POST request with formsubmit.co.

---
+++ src/components/contact/picture/ContactPicture.tsx
@@ -0,0 +1,11 @@
+export function ContactPicture () {
+  return (
+    <div className='hidden md:block'>
+      <img
+        className='rounded-2xl h-full object-cover'
+        src='https://arrlex.com/wp-content/uploads/2023/10/3426526.jpg'
+        alt='Software Developer working at his desk.'
+        />
+    </div>
+  )
+}
