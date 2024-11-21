978776efc68dd868819d1a356470b24353fc82cf
Enrique Ajin <enriquesaon@gmail.com>
2024-11-21 22:46:07 +0100

    Fix header's z-index when on mobile since the menu didnt come up. Change menu items' hover color for primaryBlue.

--- src/components/header/navigation/NavElements.tsx
+++ src/components/header/navigation/NavElements.tsx
@@ -4,8 +4,8 @@ import { NavItem } from "./NavItem";
 export function NavElements ({ isMobileMenuOpen }: { isMobileMenuOpen: boolean }) {
   const menuClassName = isMobileMenuOpen ? 'top-24' : 'top-[-100%]';
   return(
-    <div className={`${menuClassName} w-full lg:w-auto nav-links duration-200 lg:static absolute min-h-[40vh] lg:min-h-fit bg-primaryBlue lg:bg-darkBlue -z-10 lg:z-0 left-0 flex items-center justify-center`}>
-      <ul className='flex lg:flex-row flex-col gap-x-10 items-center text-gray-300 justify-evenly gap-y-10'>
+    <div className={`${menuClassName} w-full lg:w-auto nav-links duration-200 lg:static absolute min-h-[45vh] lg:min-h-fit bg-primaryBlue lg:bg-darkBlue z-10 lg:z-0 left-0 flex items-center justify-center`}>
+      <ul className='flex lg:flex-row flex-col gap-x-10 items-center text-white font-bold justify-evenly gap-y-10'>
         {
           navOptions.map((option) => {
             return (
--- src/components/header/navigation/NavItem.tsx
+++ src/components/header/navigation/NavItem.tsx
@@ -5,7 +5,7 @@ export function NavItem ({ option }: {option: NavOptions}) {
   return (
     <>
       <a href={`#${option.name}`}>
-        <li key={option.name} className="transition flex items-center gap-x-2 px-3 py-3 rounded-2xl hover:bg-blue-800 hover:text-white stroke-transparent">
+        <li key={option.name} className="transition flex items-center gap-x-2 px-3 py-3 rounded-2xl hover:bg-primaryBlue duration-500 hover:text-white stroke-transparent">
           <Icon className="w-6 h-6" />
           <p  className='transition'>{option.name}</p>
         </li>
