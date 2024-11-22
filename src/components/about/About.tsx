fe5ac0a3b07c1da859eb03f592246be109291791
Enrique Ajin <enriquesaon@gmail.com>
2024-11-22 12:34:02 +0100

    Add graduation photo. Edit About cards.

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -7,29 +7,31 @@ export function About () {
         About me
       </h1>
       {/* Grid cointainer */}
-      <div className='w-[90%] lg:w-[70%] grid gap-7 sm:grid-cols-2 my-16'>
+      <div className='w-[90%] lg:w-[70%] grid gap-7 sm:grid-cols-5 my-16'>
         {/* Experience text */}
-        <div className='w-full min-h-64 flex flex-col px-5 py-7 gap-y-3 bg-primaryPurple rounded-xl justify-between'>
+        <div className='col-span-3 w-full min-h-96 flex flex-col px-5 py-12 bg-primaryPurple rounded-xl justify-evenly'>
           <p className='font-bold text-2xl'>Experience</p>
-          <div className='flex items-center gap-x-3'>
-            <FaCheckCircle className='w-8 h-8 fill-primaryBlue rounded-full' />
-            <div className='flex flex-col'>
-              <span>Android Developer</span>
-              <span>Digital Geko 🇬🇹</span>
-              <span>2021-2024</span>
+          <div className='flex flex-col gap-y-5'>
+            <div className='flex items-start gap-x-3'>
+              <FaCheckCircle className='min-w-8 h-8 fill-primaryBlue rounded-full' />
+              <div className='flex flex-col'>
+                <span>Android Developer</span>
+                <strong>Digital Geko 🇬🇹</strong>
+                <span>2021-2024</span>
+              </div>
             </div>
-          </div>
-          <div className='flex items-center gap-x-3'>
-            <FaCheckCircle className='min-w-8 min-h-8 fill-primaryBlue rounded-full' />
-            <div className='flex flex-col'>
-              <span>Software Developer</span>
-              <span>Freelancer</span>
-              <span>present</span>
+            <div className='flex items-start gap-x-3'>
+              <FaCheckCircle className='min-w-8 min-h-8 fill-primaryBlue rounded-full' />
+              <div className='flex flex-col'>
+                <span>Software Developer</span>
+                <strong>Freelancer</strong>
+                <span>present</span>
+              </div>
             </div>
           </div>
         </div>
         {/* Experience picture */}
-        <div className='hidden sm:block w-full max-h-64'>
+        <div className='col-span-2 hidden sm:block w-full max-h-96'>
           <img 
             className='w-full h-full rounded-xl object-cover'
             src='https://thumbs.dreamstime.com/b/horario-de-trabajo-ambiente-de-trabajo-79429894.jpg'
@@ -37,27 +39,42 @@ export function About () {
           />
         </div>
         {/* Education picture */}
-        <div className='hidden sm:block w-full max-h-64'>
+        <div className='col-span-2 hidden sm:block w-full max-h-96'>
           <img 
-            className='w-full h-full rounded-xl object-cover'
-            src='https://static.vecteezy.com/system/resources/previews/002/245/412/non_2x/online-graduation-ceremony-in-mobile-phone-free-vector.jpg'
+            className='w-full h-full rounded-xl object-cover object-top'
+            // src='https://static.vecteezy.com/system/resources/previews/002/245/412/non_2x/online-graduation-ceremony-in-mobile-phone-free-vector.jpg'
+            src='../public/enrique-graduation.jpg'
             alt='Illustration of people at a office.'
           />
         </div>
         {/* Education */}
-        <div className='w-full min-h-64 flex flex-col px-5 py-7 gap-y-3 bg-primaryPurple rounded-xl justify-between'>
+        <div className='col-span-3 w-full min-h-96 flex flex-col px-5 py-7 gap-y-3 bg-primaryPurple rounded-xl justify-evenly'>
           <p className='font-bold text-2xl'>Education</p>
-          <div className='flex items-center gap-x-3'>
-            <FaCheckCircle className='min-w-8 min-h-8 fill-primaryBlue rounded-full' />
-            <p>Studying a Master’s degree in Data Analysis</p>
-          </div>
-          <div className='flex items-center gap-x-3'>
-            <FaCheckCircle className='min-w-8 min-h-8 fill-primaryBlue rounded-full' />
-            <p>Studied Computer Science</p>
-          </div>
-          <div className='flex items-center gap-x-3'>
-            <FaCheckCircle className='min-w-8 min-h-8 fill-primaryBlue rounded-full' />
-            <p>Self-taught developer</p>
+          <div className='flex flex-col gap-y-5'>
+            <div className='flex items-start gap-x-3'>
+              <FaCheckCircle className='min-w-8 min-h-8 fill-primaryBlue rounded-full' />
+              <div>
+                <p>Master's in Data Analysis & Business Intelligence</p>
+                <strong>Universidad Panamericana 🇬🇹</strong>
+                <p>Ongoing</p>
+              </div>
+            </div>
+            <div className='flex items-start gap-x-3'>
+              <FaCheckCircle className='min-w-8 min-h-8 fill-primaryBlue rounded-full' />
+              <div>
+                <p>Bachelor's in Computer Science & IT</p>
+                <strong>Universidad Panamericana 🇬🇹</strong>
+                <p>2019-2023</p>
+              </div>
+            </div>
+            <div className='flex items-start gap-x-3'>
+              <FaCheckCircle className='min-w-8 min-h-8 fill-primaryBlue rounded-full' />
+              <div>
+                <p>University Technician in Systems & IT</p>
+                <strong>Universidad Panamericana 🇬🇹</strong>
+                <p>2019-2021</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
