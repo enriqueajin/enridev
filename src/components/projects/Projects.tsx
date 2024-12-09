214175781652815a77d75ccf9c954161e45e20ae
Enrique Ajin <enriquesaon@gmail.com>
2024-12-09 17:16:58 +0100

    Make Projects' section block take up h-screen when lg.

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -2,7 +2,10 @@ import { ProjectGrid } from "./lists/ProjectGrid";
 
 export function Projects () {
   return (
-    <section id='Projects' className='flex flex-col w-full bg-secondaryDarkBlue text-white items-center'>
+    <section 
+      id='Projects' 
+      className='flex flex-col lg:h-screen bg-secondaryDarkBlue text-white items-center justify-center'
+    >
       <h1 className='font-extrabold text-balance text-3xl md:text-4xl lg:text-5xl leading-tight mt-10'>
         My projects
       </h1>
